cf0e23260dd259efc170cf201da2bca924b80e40
xl767 <xl767@cornell.edu>
2022-11-28 18:57:21 -0500

    Weekly update

--- app/src/main/java/com/example/a5725finalproject/Activity2.java
+++ app/src/main/java/com/example/a5725finalproject/Activity2.java
@@ -2,7 +2,12 @@ package com.example.a5725finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        //String vPath = "android.resource://" + getPackageName() + "/raw/boids";
+//        String vPath = "https://www.google.com";
+        String vPath = "http://10.49.10.99:8000";
+
+
+        WebView webView = findViewById(R.id.wv);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(vPath);
+
+
     }
 }
