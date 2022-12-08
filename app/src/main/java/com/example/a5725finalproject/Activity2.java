17424a32881e935e6d25c7e26484b46d4975317d
xl767 <xl767@cornell.edu>
2022-12-08 12:18:00 -0500

    New Update

--- app/src/main/java/com/example/a5725finalproject/Activity2.java
+++ app/src/main/java/com/example/a5725finalproject/Activity2.java
@@ -4,12 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Activity2 extends AppCompatActivity {
+    private Button right;
+    private Button left;
+    private Button forward;
+    private Button backward;
+
+    PrintWriter pw;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +34,39 @@ public class Activity2 extends AppCompatActivity {
 
         //String vPath = "android.resource://" + getPackageName() + "/raw/boids";
 //        String vPath = "https://www.google.com";
-        String vPath = "http://10.49.10.99:8000";
 
+        String vPath = "http://10.49.10.99:8000";
 
         WebView webView = findViewById(R.id.wv);
         webView.setWebViewClient(new WebViewClient());
-
         webView.loadUrl(vPath);
+
+        forward = (Button) findViewById(R.id.button1);
+        left = (Button) findViewById(R.id.button2);
+        backward = (Button) findViewById(R.id.button4);
+        right = (Button) findViewById(R.id.button3);
+
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg;
+                try {
+                    Socket s = new Socket("10.49.10.99", 5000);
+                    pw = new PrintWriter(s.getOutputStream());
+                    pw.println("123");
+                    System.out.println("123");
+                    pw.flush();
+                    InputStreamReader ir = new InputStreamReader(s.getInputStream());
+                    BufferedReader br = new BufferedReader(ir);
+                    msg = br.readLine();
+                    System.out.println(msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
 
 
     }
--- app/src/main/java/com/example/a5725finalproject/MainActivity.java
+++ app/src/main/java/com/example/a5725finalproject/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            //your codes here
+
+        }
 
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
